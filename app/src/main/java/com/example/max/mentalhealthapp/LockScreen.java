d87ee26bd1330def0b9ee3beac14086536105832
Max Nguyen <maxnguyen@student.hcps.org>
2017-11-21 12:08:59 -0500

    Clicking the button no longer crashes the app.

--- app/src/main/java/com/example/max/mentalhealthapp/LockScreen.java
+++ app/src/main/java/com/example/max/mentalhealthapp/LockScreen.java
@@ -2,12 +2,28 @@ package com.example.max.mentalhealthapp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class LockScreen extends AppCompatActivity {
+Button imageButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lock_screen);
+
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+        imageButton = (Button) findViewById(R.id.button1);
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
     }
 }
