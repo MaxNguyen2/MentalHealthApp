90798143fafccf5159951a15978991d69242ae3f
Max Nguyen <maxnguyen@student.hcps.org>
2018-02-02 11:27:36 -0500

    Time picker dialog is now in spinner mode. Starting to add spinners. Added comments. Will be trying to follow Material Guidelines more closely.

--- app/src/main/java/com/example/max/mentalhealthapp/MoodMonitoring.java
+++ app/src/main/java/com/example/max/mentalhealthapp/MoodMonitoring.java
@@ -6,7 +6,6 @@ import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
-import android.util.Log;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
@@ -14,28 +13,32 @@ import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 
+//Page where user decides whether to report a mood or view mood graphs
 public class MoodMonitoring extends AppCompatActivity {
-     ListView mDrawerList;
-     ArrayAdapter<String> mAdapter;
-     Intent myIntent;
+    ListView mDrawerList;
+    ArrayAdapter<String> mAdapter;
+    Intent myIntent;
     ActionBarDrawerToggle mDrawerToggle;
-     DrawerLayout mDrawerLayout;
+    DrawerLayout mDrawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_monitoring);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //sets up navigation drawer
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         setupDrawer();
         mDrawerList = (ListView) findViewById(R.id.navList);
         addDrawerItems();
 
+        //when button is clicked, goes to mood reporting page
         Button button = (Button) findViewById(R.id.reportButton);
         button.setOnClickListener(new View.OnClickListener() {
-            public void onClick (View v)
-            {
+            public void onClick(View v) {
                 myIntent = new Intent(MoodMonitoring.this, MoodReporting.class);
                 MoodMonitoring.this.startActivity(myIntent);
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
@@ -43,12 +46,14 @@ public class MoodMonitoring extends AppCompatActivity {
         });
     }
 
+    //sets up navigation list with the pages for the different features
     public void addDrawerItems() {
         String[] navArray = {"Home", "Mood Monitoring", "Safety Plan", "Breathing Exercises", "Crisis Lines", "Information", "Settings"};
         mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, navArray);
         mDrawerList.setAdapter(mAdapter);
         mDrawerList.bringToFront();
 
+        //takes user to page of feature that they click on
         mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
@@ -65,24 +70,26 @@ public class MoodMonitoring extends AppCompatActivity {
         });
     }
 
-     public void setupDrawer () {
-        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {};
-        mDrawerToggle.syncState();
+    //helps set up navigation drawer to be accessed by toolbar
+    public void setupDrawer() {
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout, R.string.drawer_open, R.string.drawer_close) {
+        };
+        mDrawerToggle.syncState(); //needed to make hamburger icon functional
         mDrawerToggle.setDrawerIndicatorEnabled(true);
         mDrawerLayout.addDrawerListener(mDrawerToggle);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         getSupportActionBar().setHomeButtonEnabled(true);
         mDrawerToggle.setDrawerIndicatorEnabled(true);
-        mDrawerToggle.setHomeAsUpIndicator(R.drawable.ic_action_name);
+        mDrawerToggle.setHomeAsUpIndicator(R.drawable.ic_action_name); //specifies hamburger icon
     }
 
     @Override
-    public boolean onOptionsItemSelected (MenuItem item) {
-        if(mDrawerToggle.onOptionsItemSelected(item)) {
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (mDrawerToggle.onOptionsItemSelected(item)) {
             return true;
         }
         return super.onOptionsItemSelected(item);
-        }
     }
+}
 
 
--- app/src/main/java/com/example/max/mentalhealthapp/MoodReporting.java
+++ app/src/main/java/com/example/max/mentalhealthapp/MoodReporting.java
@@ -1,56 +1,67 @@
 package com.example.max.mentalhealthapp;
 
-import android.app.AlertDialog;
 import android.app.DatePickerDialog;
 import android.app.TimePickerDialog;
-import android.content.DialogInterface;
-import android.content.Intent;
+import android.graphics.Color;
 import android.icu.util.Calendar;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.ListView;
-import android.widget.TextClock;
 import android.widget.TextView;
 import android.widget.TimePicker;
 
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
+//page for users to make a report on their mood
 public class MoodReporting extends MoodMonitoring {
- TextView dateText;
+    TextView dateText;
     Calendar myCalendar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mood_reporting);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        //sets up navigation drawer
         mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
         setupDrawer();
         mDrawerList = (ListView) findViewById(R.id.navList);
         addDrawerItems();
 
-        myCalendar = Calendar.getInstance();
-        dateText= (TextView) findViewById(R.id.dateText);
-        updateLabel();
-        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
+        //sets status bar color to be dark orange
+        Window window = getWindow();
+        window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+        window.setStatusBarColor(Color.rgb(245, 124, 0));
 
+
+        myCalendar = Calendar.getInstance(); //gets current time and date information
+        dateText = (Button) findViewById(R.id.dateButton); //button that displays date
+        updateLabel(); //displays current date before a different date is selected
+
+        //when date is selected in dialog, new date is displayed
+        final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
-            public void onDateSet(DatePicker view, int year, int monthOfYear,
-                                  int dayOfMonth) {
+            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
                 myCalendar.set(Calendar.YEAR, year);
                 myCalendar.set(Calendar.MONTH, monthOfYear);
                 myCalendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
                 updateLabel();
             }
         };
-        dateText.setOnClickListener(new View.OnClickListener() {
 
+        //opens date picker dialog when button is clicked
+        dateText.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new DatePickerDialog(MoodReporting.this, date, myCalendar
@@ -59,34 +70,38 @@ public class MoodReporting extends MoodMonitoring {
             }
         });
 
-        final TextClock textClock = (TextClock) findViewById(R.id.textClock);
+        final Button textClock = (Button) findViewById(R.id.timeButton); //button that displays the time
+        textClock.setText(new SimpleDateFormat("h:mm a", Locale.US).format(myCalendar.getTime())); //displays current time
+
+        //when button is clicked, opens time picker dialog which updates button text with user selected time
         textClock.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Calendar mcurrentTime = Calendar.getInstance();
+                Calendar mcurrentTime = Calendar.getInstance(); //gets current time
                 int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
                 int minute = mcurrentTime.get(Calendar.MINUTE);
-                TimePickerDialog mTimePicker;
-                mTimePicker = new TimePickerDialog(MoodReporting.this, android.R.style.Theme_Material_Light_Dialog_Alert, new TimePickerDialog.OnTimeSetListener() {
+                TimePickerDialog mTimePicker = new TimePickerDialogFixedNougatSpinner(MoodReporting.this, new TimePickerDialog.OnTimeSetListener() {
                     @Override
                     public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
-                        textClock.setText( selectedHour + ":" + selectedMinute);
+                        boolean isPM = (selectedHour >= 12);
+                        textClock.setText(String.format("%d:%02d %s", (selectedHour == 12 || selectedHour == 0) ? 12 : selectedHour % 12, selectedMinute, isPM ? "PM" : "AM")); //sets text to be selected time
                     }
+
                 }, hour, minute, false);
-                mTimePicker.setTitle("Select Time");
+                mTimePicker.setTitle("Select Time"); //title of time picker dialog
                 mTimePicker.show();
 
             }
         });
     }
 
+    //used to update date displayed in button
     private void updateLabel() {
         String myFormat = "MM/dd/yy";
         SimpleDateFormat sdf = new SimpleDateFormat(myFormat, Locale.US);
 
         dateText.setText(sdf.format(myCalendar.getTime()));
     }
-
 
 
 }
